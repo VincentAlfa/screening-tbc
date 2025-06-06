437f07b023c6a230c2a6d43c4353f9be41548cad
VincentAlfa <vincent111232@gmail.com>
2025-06-06 19:02:14 +0700

    style: update AboutCard component styles for improved consistency

--- src/components/about-card/about-card.tsx
+++ src/components/about-card/about-card.tsx
@@ -12,14 +12,14 @@ export function AboutCard({ title, content, className }: AboutCardProps) {
   return (
     <Card
       className={cn(
-        'flex h-full min-h-[350px] w-full flex-col p-0 gap-0 bg-[#DDF6D2] border-0', 
-        className
+        'flex h-full min-h-[350px] w-full flex-col gap-0 border-0 bg-[#DDF6D2] p-0',
+        className,
       )}
     >
       <CardHeader className='rounded-t-xl bg-[#ECFAE5] p-1'>
         <CardTitle className='text-center text-lg'>{title}</CardTitle>
       </CardHeader>
-      <CardContent className='flex flex-1 items-start bg-[#DDF6D2] pb-4 text-base'>
+      <CardContent className='bg-secondary-green flex flex-1 items-start pb-4 text-base'>
         {content}
       </CardContent>
     </Card>
